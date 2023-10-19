b513b694662e2fae7f3b52ea630eb848d963ec78
Frederik Rabøl <frm@cardlay.com>
2023-10-19 09:58:57 +0200

    fix some vuex problems

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { createApp } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import App from "./App.vue";
 import router from "./router";
-import Vuex from "vuex";
 
 import "./assets/main.css";
 import store from "./store";
 
 const app = createApp(App);
-app.use(store);
 app.use(router);
+app.use(store);
 app.use(VueQueryPlugin).mount("#app");
--- src/store/modules/tags.ts
+++ src/store/modules/tags.ts
@@ -1,32 +1,36 @@
-import { createStore } from "vuex";
+import type { GithubTag } from "@/api/github";
+import type { ActionContext } from "vuex";
 
 interface State {
   selectedTags: string[];
 }
 
-const store = createStore<State>({
+const store = {
   state: () => ({
     selectedTags: [],
   }),
   getters: {
-    selectedTags: (state) => state.selectedTags,
+    selectedTags: (state: State) => state.selectedTags,
   },
   actions: {
-    setSelectedTags({ commit }, tags) {
+    setSelectedTags(
+      { commit }: ActionContext<State, {}>,
+      tags: GithubTag["name"][]
+    ) {
       commit("setSelectedTags", tags);
     },
-    resetTags({ commit }) {
+    resetTags({ commit }: ActionContext<State, {}>) {
       commit("resetTags");
     },
   },
   mutations: {
-    setSelectedTags(state, tags) {
+    setSelectedTags(state: State, tags: GithubTag["name"][]) {
       state.selectedTags = tags;
     },
-    resetTags: (state) => {
+    resetTags: (state: State) => {
       state.selectedTags = [];
     },
   },
-});
+};
 
 export default store;
